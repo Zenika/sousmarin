5c33da214cd410c7c91b8117ee7fbf6e5e789c48
Guillaume Membré <guillaume.membre@zenika.com>
2022-09-25 21:38:18 +0200

    Update animation

--- row_led_blink/row_led_blink.ino
+++ row_led_blink/row_led_blink.ino
@@ -2,42 +2,73 @@ const byte ledPin[] = { 2, 3, 4, 5, 6, 7, 8 };
 byte ledCount = sizeof(ledPin) / sizeof(ledPin[0]);
 
 void setup() {
-  Serial.begin(9600);
-  Serial.flush();
+//  Serial.begin(9600);
+//  Serial.flush();
   for (byte index = 0; index < ledCount; index++) {
     pinMode(ledPin[index], OUTPUT);
   }
 }
 
 byte number = 0b011;
-byte window_size = 1;
 void loop() {
   //byte row[] = { 1, 0, 1 };
   //display(row);
 
   display_binary(number);
-  animation_k2000();
-  delay(100);
+  //animation_k2000();
+  animation_table();
+
 }
 
-byte anim_direction = 1;
-byte anim_count = 0;
-byte anim_count_max = 5;
+int table_anim_ledTime[] = {-10, 10, -5, 30, -20, 20, 5};
+
+void animation_table() {
+  for (byte index = 0; index < ledCount; index++) {
+    digitalWrite(ledPin[index], table_anim_ledTime[index] > 0);
+    
+    if (table_anim_ledTime[index] > 0) {
+      table_anim_ledTime[index] = table_anim_ledTime[index] - 1;
+      if (table_anim_ledTime[index] == 0) {
+        byte value = random(50);
+        table_anim_ledTime[index] = -value;
+      }
+    } else {
+      table_anim_ledTime[index] = table_anim_ledTime[index] + 1;
+      if (table_anim_ledTime[index] == 0) {
+        byte value = random(50);
+        table_anim_ledTime[index] = value;
+      }
+    }
+
+  }
+  delay(200);
+}
+
+byte k2000_anim_direction = 0;
+byte k2000_anim_count = 0;
+byte k2000_anim_count_max = 5;
 
 void animation_k2000() {
-  if (anim_direction == 2) {
-    number = number >> 1;
-    anim_count --;
-    if (anim_count == 0) {
-      anim_direction = 1;
-    }
-  } else {
-    number = number << 1;
-    anim_count ++;
-    if (anim_count == anim_count_max) {
-      anim_direction = 2;
-    }
+  switch (k2000_anim_direction) {
+    case 0:
+      number = 0b011;
+      k2000_anim_direction = 1;
+    case 1:
+      number = number << 1;
+      k2000_anim_count ++;
+      if (k2000_anim_count == k2000_anim_count_max) {
+        k2000_anim_direction = 2;
+      }
+      break;
+    case 2:
+      number = number >> 1;
+      k2000_anim_count --;
+      if (k2000_anim_count == 0) {
+        k2000_anim_direction = 1;
+      }
+      break;
   }
+  delay(100);
 }
 
 void display_table(byte row[]) {
