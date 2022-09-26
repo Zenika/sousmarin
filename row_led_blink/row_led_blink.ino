0d333edc7bfc7a313c0c0ba79bccbef329bbc21e
Guillaume Membré <guillaume.membre@zenika.com>
2022-09-26 10:23:46 +0200

    ajout de la vitesse d'animation

--- row_led_blink/row_led_blink.ino
+++ row_led_blink/row_led_blink.ino
@@ -1,31 +1,54 @@
-const byte ledPin[] = { 2, 3, 4, 5, 6, 7, 8 };
+#include <ezButton.h>
+
+const byte ledPin[] = { 4, 5, 6, 7, 8, 9, 10 };
+ezButton button(2);
+byte potPin = A1;
+
 byte ledCount = sizeof(ledPin) / sizeof(ledPin[0]);
 
 void setup() {
-//  Serial.begin(9600);
-//  Serial.flush();
+  Serial.begin(9600);
+  Serial.flush();
   for (byte index = 0; index < ledCount; index++) {
     pinMode(ledPin[index], OUTPUT);
   }
+  pinMode(potPin, INPUT);
 }
 
-byte number = 0b011;
+byte current_animation = 0;
+#define max_animation 2
+
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+int speed_animation = 30;
 void loop() {
-  //byte row[] = { 1, 0, 1 };
-  //display(row);
+  button.loop();
 
-  display_binary(number);
-  //animation_k2000();
-  animation_table();
+  if (button.isPressed()) {
+    current_animation = (current_animation + 1) % max_animation ;
+  }
 
+  speed_animation = map(analogRead(potPin), 0, 1023, 50, 600);
+  
+  switch (current_animation) {
+    case 0:
+      animation_table();
+      break;
+    case 1:
+      animation_k2000();
+      break;
+  }
 }
 
-int table_anim_ledTime[] = {-10, 10, -5, 30, -20, 20, 5};
+int table_anim_ledTime[] = { -10, 10, -5, 30, -20, 20, 5};
 
 void animation_table() {
   for (byte index = 0; index < ledCount; index++) {
     digitalWrite(ledPin[index], table_anim_ledTime[index] > 0);
-    
+
     if (table_anim_ledTime[index] > 0) {
       table_anim_ledTime[index] = table_anim_ledTime[index] - 1;
       if (table_anim_ledTime[index] == 0) {
@@ -41,34 +64,36 @@ void animation_table() {
     }
 
   }
-  delay(200);
+  delay(speed_animation * 4);
 }
 
 byte k2000_anim_direction = 0;
 byte k2000_anim_count = 0;
 byte k2000_anim_count_max = 5;
+byte k2000_anim_number = 0b011;
 
 void animation_k2000() {
   switch (k2000_anim_direction) {
     case 0:
-      number = 0b011;
+      k2000_anim_number = 0b011;
       k2000_anim_direction = 1;
     case 1:
-      number = number << 1;
+      k2000_anim_number = k2000_anim_number << 1;
       k2000_anim_count ++;
       if (k2000_anim_count == k2000_anim_count_max) {
         k2000_anim_direction = 2;
       }
       break;
     case 2:
-      number = number >> 1;
+      k2000_anim_number = k2000_anim_number >> 1;
       k2000_anim_count --;
       if (k2000_anim_count == 0) {
         k2000_anim_direction = 1;
       }
       break;
   }
-  delay(100);
+  display_binary(k2000_anim_number);
+  delay(speed_animation);
 }
 
 void display_table(byte row[]) {
