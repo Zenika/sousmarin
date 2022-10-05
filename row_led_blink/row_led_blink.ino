3b9d27b6a4f3be550ab735967e4ae1c8f7ea0da9
Guillaume Membré <guillaume.membre@zenika.com>
2022-10-05 23:20:23 +0200

    add more animation and led

--- row_led_blink/row_led_blink.ino
+++ row_led_blink/row_led_blink.ino
@@ -1,29 +1,28 @@
 #include <ezButton.h>
 
-const byte ledPin[] = { 4, 5, 6, 7, 8, 9, 10 };
-ezButton button(2);
-byte potPin = A1;
+#define LED_ON 1
+#define LED_OFF 0
 
-byte ledCount = sizeof(ledPin) / sizeof(ledPin[0]);
+const byte ledPin[] = { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16, 17, 18, 19, 2, 15 };
+ezButton button(14);
+byte potPin = A7;
+
+byte led_count = sizeof(ledPin) / sizeof(ledPin[0]);
 
 void setup() {
   Serial.begin(9600);
   Serial.flush();
-  for (byte index = 0; index < ledCount; index++) {
+  for (byte index = 0; index < led_count; index++) {
     pinMode(ledPin[index], OUTPUT);
   }
   pinMode(potPin, INPUT);
 }
 
-byte current_animation = 0;
-#define max_animation 2
+byte current_animation = 4;
+#define max_animation 3
+bool toggle_led = true;
+int speed_animation = 0;
 
-int buttonState;             // the current reading from the input pin
-int lastButtonState = LOW;   // the previous reading from the input pin
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
-
-int speed_animation = 30;
 void loop() {
   button.loop();
 
@@ -31,8 +30,11 @@ void loop() {
     current_animation = (current_animation + 1) % max_animation ;
   }
 
-  speed_animation = map(analogRead(potPin), 0, 1023, 50, 600);
-  
+  speed_animation = map(analogRead(potPin), 0, 1023, 60, 600);
+
+  toggle_led = !toggle_led;
+  digitalWrite(LED_BUILTIN, toggle_led);
+
   switch (current_animation) {
     case 0:
       animation_table();
@@ -40,13 +42,51 @@ void loop() {
     case 1:
       animation_k2000();
       break;
+    case 2:
+      animation_linear(false);
+      break;
+    case 3:
+      animation_linear(true);
+      break;
+    case 4:
+      animation_table();
+      animation_linear(true);
+      break;
   }
 }
 
-int table_anim_ledTime[] = { -10, 10, -5, 30, -20, 20, 5};
+int linear_anim_index = 0;
+int linear_anim_dir = 1;
+int linear_nb_led = 12;
+void animation_linear(bool reverse) {
+  if (linear_anim_index >= linear_nb_led && linear_anim_dir == 1) {
+    if (reverse) {
+      linear_anim_dir = -1;
+    } else {
+      for (byte index = 0; index < linear_nb_led; index++) {
+        digitalWrite(ledPin[index], LED_OFF);
+      }
+    }
+  }
+  if (linear_anim_dir == 1) {
+    digitalWrite(ledPin[linear_anim_index], LED_ON);
+  } else {
+    digitalWrite(ledPin[linear_anim_index], LED_OFF);
+  }
+  linear_anim_index = linear_anim_index + linear_anim_dir;
+  if (linear_anim_index == -1) {
+    linear_anim_index = 0;
+    linear_anim_dir = 1;
+  }
+  delay(speed_animation);
+}
 
+int table_anim_ledTime[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -10, 10, -5, 30};
+
+int table_nb_led = 4;
+int table_led_start = 12;
 void animation_table() {
-  for (byte index = 0; index < ledCount; index++) {
+  for (byte index = table_led_start; index < table_led_start + table_nb_led; index++) {
     digitalWrite(ledPin[index], table_anim_ledTime[index] > 0);
 
     if (table_anim_ledTime[index] > 0) {
@@ -64,23 +104,24 @@ void animation_table() {
     }
 
   }
-  delay(speed_animation * 4);
+  delay(speed_animation);
 }
 
 byte k2000_anim_direction = 0;
-byte k2000_anim_count = 0;
-byte k2000_anim_count_max = 5;
-byte k2000_anim_number = 0b011;
+int k2000_anim_count = 0;
+#define k2000_led_count 13
+#define k2000_led_on 1
+uint16_t k2000_anim_number = 0b0;
 
 void animation_k2000() {
   switch (k2000_anim_direction) {
     case 0:
-      k2000_anim_number = 0b011;
+      k2000_anim_number = 0b0011;
       k2000_anim_direction = 1;
     case 1:
       k2000_anim_number = k2000_anim_number << 1;
       k2000_anim_count ++;
-      if (k2000_anim_count == k2000_anim_count_max) {
+      if (k2000_anim_count + 1 == k2000_led_count - k2000_led_on) {
         k2000_anim_direction = 2;
       }
       break;
@@ -92,18 +133,23 @@ void animation_k2000() {
       }
       break;
   }
-  display_binary(k2000_anim_number);
+  display_binary(k2000_anim_number, k2000_led_count);
   delay(speed_animation);
 }
 
 void display_table(byte row[]) {
-  for (byte index = 0; index < ledCount; index++) {
+  for (byte index = 0; index < led_count; index++) {
     digitalWrite(ledPin[index], row[index]);
   }
 }
 
-void display_binary(byte number_to_display) {
-  for (byte index = 0; index < ledCount; index++) {
+void display_binary(uint16_t number_to_display, int nb_led) {
+  for (byte index = 0; index < nb_led - 1; index++) {
+    /*    Serial.print("led ");
+        Serial.print(ledPin[index]);
+        Serial.print(" ");
+        Serial.println(bitRead(number_to_display, index) ? "o" : " "); */
+
     digitalWrite(ledPin[index], bitRead(number_to_display, index));
   }
 }
